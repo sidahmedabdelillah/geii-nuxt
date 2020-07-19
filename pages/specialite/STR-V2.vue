347dcca8f6f6e0df7adf53dd081f14b9143a83c7
sidahmedabdelillah <s_bentegar@enst.dz>
2020-07-19 21:35:59 +0100

    telecome v2

---
+++ pages/specialite/STR-V2.vue
@@ -0,0 +1,130 @@
+<template> 
+<div >
+    	<!-- banner -->
+	<div class="banner_w3lspvt">
+		<div class="csslider infinity" id="slider1">			
+			<ul class="banner banner1">
+				<li class="banner-top1">
+					<div class="container">
+						<div class="banner-info_w3ls">
+						
+							<h3 class="text-wh font-weight-bold mt-2 mb-5">Systeme Telecommunication 
+								<br>et Resaux </h3>
+							<p> La spécialité Systèmes de Telecommunications et Réseaux STR forme des ingénieurs compétents dans les domaines de la communication et des réseaux qu’ils soient informatiques ou téléphoniques, fixes ou mobiles, maitrisant les aspects scientifiques, technologiques et organisationnels permettant de mettre en œuvre des stratégies telle que la sécurité des communications, des applications IoT ,etc. </p>
+						</div>
+					</div>
+				</li>
+			
+			</ul>
+			
+		</div>
+	</div>
+	
+	<section class="about-bottom" id="services">
+		<div class="container pb-lg-4">
+			<div class="row bg-colors text-center">
+				<div class="col-md-4 service-subgrids">
+					<div class="w3ls-about-grid py-lg-5 py-md-4 py-5 px-3">
+						<h4 class="text-wh font-weight-bold mb-3">Periode de foramtion</h4>
+						<dd class="text-li"> Full-time: 3 ans</dd>
+					</div>
+					
+				</div>
+				<div class="col-md-4 service-subgrids bg-li">
+					<div class="w3ls-about-grid py-lg-5 py-md-4 py-5 px-3">
+						<h4 class="text-bl font-weight-bold mb-3"> Type de foramtion </h4>
+						<dd class="text-secondary"> Foramtion ingenieur
+						</dd>
+						<dd class="text-secondary">
+                  Formaion complementaire master
+				</dd>
+				
+
+					</div>
+					
+				</div>
+				<div class="col-md-4 service-subgrids">
+					<div class="w3ls-about-grid py-lg-5 py-md-4 py-5 px-3">
+						<h4 class="text-wh font-weight-bold mb-3">Methode dacess</h4>
+						<p class="text-li"> Concour daccess aux ecole superieur</p>
+					</div>
+					
+				</div>
+			</div>
+			
+		</div>
+	</section>
+	<!-- //banner bottom grids -->
+
+	<!-- about -->
+	<section class="w3ls-bnrbtm py-5" id="about">
+		
+		<div class="container py-xl-3 py-lg-1">
+			<div class="row">
+				<div class="col-lg-8 pr-xl-2 ">
+					<h3 class="text-colors let-spa font-weight-bold ">Introduction au Programme</h3>
+					<p class="text-bl" > The BA gives you the freedom to follow your interests and study
+						the subjects you love You can choose from a wide range of study
+						possibilities that cater for many different interests or career
+						need to future-proof your career. Capabilities such as goals.
+						Your BA study can develop the knowledge and skills you for
+						different opportunities in the fast-changing world of work.
+						communication and creative thinking will keep your options open
+						one or two of our optional, theme-based modules. Your double You
+						can further extend your skill set for future careers through
+						knowledge and skills. You have the flexibility to explore major
+						will give your degree a strong focus and deepen your different
+						subjects before you choose your two majors. </p>
+						<h3 class="text-colors let-spa font-weight-bold ">Introduction au Programme</h3>
+					<p class="text-bl" > The BA gives you the freedom to follow your interests and study
+						the subjects you love You can choose from a wide range of study
+						possibilities that cater for many different interests or career
+						need to future-proof your career. Capabilities such as goals.
+						Your BA study can develop the knowledge and skills you for
+						different opportunities in the fast-changing world of work.
+						communication and creative thinking will keep your options open
+						one or two of our optional, theme-based modules. Your double You
+						can further extend your skill set for future careers through
+						knowledge and skills. You have the flexibility to explore major
+						will give your degree a strong focus and deepen your different
+						subjects before you choose your two majors. </p>
+						
+				</div>
+				<div class="col-lg-4">
+					<div class="row align-items-end">
+						<div class="mission" >
+						  <h1>Notre mission</h1>
+						  <p class="text-wh">Doter les eleve dune capitale de connaissance  dune demarch qui leur permet de :
+							<ul>
+							  <li class="text-wh">Lancer des projet PME PMI</li>
+							  <li>Integre des etreproise industruel</li>
+							  <li>Acceder au departement recherch</li>
+							</ul>
+							</p>
+						</div>
+				    </div>
+					</div>
+						
+					
+				</div>
+			</div>
+
+				
+	</section>
+
+	
+</div>
+</template>
+
+<script>
+export default {
+    head() {
+    return { link: [{ rel: "stylesheet", href: "/css/specialite/v2.css" }] };
+  },
+};
+</script>
+
+<style  scoped>
+
+
+</style>>
