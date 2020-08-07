138cfe23d2b82c82329bc289600647d53ac645ea
sidahmedabdelillah <s_bentegar@enst.dz>
2020-08-07 18:07:50 +0100

    aded access etudiant link to navbar

--- components/navbar/menu.vue
+++ components/navbar/menu.vue
@@ -85,7 +85,7 @@ export default {
             },
             {
               name: "Access etudiants",
-              path: "#"
+              path: "/etudiant/cour/"
             }
           ]
         },
